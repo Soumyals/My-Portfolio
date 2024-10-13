fbc61dcbb94f8eeca3a8cb053cbc93e1607495f7
Soumya LS <soumyals0808@gmail.com>
2024-10-13 23:11:03 +0530

    website working

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,26 @@
 import { BrowserRouter } from "react-router-dom";
 
-import {Navbar, Hero, Experience, Works, Feedbacks, About, Contact, Tech, StarsCanvas} from './components';
+import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./component";
 
 const App = () => {
-
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
         </div>
-
         <About />
         <Experience />
         <Tech />
         <Works />
-        {/* <Feedbacks /> */}
-
-        <div className="relative z-0">
+        <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
         </div>
-
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/assets/@types/component.d.ts
@@ -0,0 +1,15 @@
+// src/@types/components.d.ts
+declare module './components' {
+    export const Navbar: React.FC<any>;
+    export const Hero: React.FC<any>;
+    export const About: React.FC<any>;
+    export const Tech: React.FC<any>;
+    export const Experience: React.FC<any>;
+    export const Works: React.FC<any>;
+    export const Feedbacks: React.FC<any>;
+    export const Contact: React.FC<any>;
+    export const EarthCanvas: React.FC<any>;
+    export const BallCanvas: React.FC<any>;
+    export const ComputersCanvas: React.FC<any>;
+    export const StarsCanvas: React.FC<any>;
+}
